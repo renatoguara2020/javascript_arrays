499fec3b43f1c7c3a3fe2415bcf7b4dfc9a9a5e1
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-27 14:41:22 -0300

    commit changes

--- scripts.js
+++ scripts.js
@@ -3,41 +3,67 @@
 
 
 
-const nomes = ['renato','nathan','creuza', 'sandra', 'risaldo', 'rubens','caroline','fernando','rubiana'];
+// const nomes = ['renato','nathan','creuza', 'sandra', 'risaldo', 'rubens','caroline','fernando','rubiana'];
 
 
-for(let i = 0; i < nomes.length; i++){
+// for(let i = 0; i < nomes.length; i++){
 
 
 
-    //console.log(nomes[i]);
-}
+//     //console.log(nomes[i]);
+// }
 
-const letras = nomes.filter((n)=>{
+// const letras = nomes.filter((n)=>{
 
-    if (n.length == 7){
+//     if (n.length == 7){
 
-        return n;
-    }
-})
+//         return n;
+//     }
+// })
 
 
-console.log(letras)
+// console.log(letras)
 
-const arrays = [1,2,3,4,5,6,7,8,9,10,11];
+// const arrays = [1,2,3,4,5,6,7,8,9,10,11];
 
-for(let i = 0; i < arrays.length; i++){
+// for(let i = 0; i < arrays.length; i++){
 
-    //console.log(arrays[i]);
+//     //console.log(arrays[i]);
 
-}
+// }
 
-const highNumbers = arrays.filter((n)=>{
+// const highNumbers = arrays.filter((n)=>{
 
-    if(n >= 4){
+//     if(n >= 4){
 
-        return n;
-    }
-})
+//         return n;
+//     }
+// })
 
 //console.log(highNumbers)
+
+const produtos = [
+
+    {name: 'Camisa', price: 10.99, category: "Roupas"},
+    {name: 'Camisa Polo', price: 109.99, category: "Roupas Esportivas"},
+    {name: 'Calça Jeans', price: 80.99, category: "Roupas"},
+    {name: 'Chaleira elétrica', price: 90.99, category: "Eletrônicos"}
+
+]
+
+produtos.map((product)=>{
+
+if(product.category === 'Roupas'){
+
+    product.onSale = true
+}
+})
+
+console.log(produtos)
+
+
+const nome = "Nathanzinho Soares";
+
+const idade = 4;
+
+console.log(`O ${nome} esta estudado e tem ${idade} anos`)
