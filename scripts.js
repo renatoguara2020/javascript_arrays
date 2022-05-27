42ca613abff79a3d2e0865f93efa9613e021ad79
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-27 15:17:29 -0300

    commit changes

--- scripts.js
+++ scripts.js
@@ -3,7 +3,13 @@
 
 
 
-// const nomes = ['renato','nathan','creuza', 'sandra', 'risaldo', 'rubens','caroline','fernando','rubiana'];
+//  const nomes = ['renato','nathan','creuza', 'sandra', 'risaldo', 'rubens','caroline','fernando','rubiana'];
+
+// const [n1,n2,n3,n4,n5,n6,n7,n8,n9] =nomes;
+
+// console.log(n1)
+// console.log(n9)
+// console.log(n5)
 
 
 // for(let i = 0; i < nomes.length; i++){
@@ -42,28 +48,40 @@
 
 //console.log(highNumbers)
 
-const produtos = [
+// const produtos = [
 
-    {name: 'Camisa', price: 10.99, category: "Roupas"},
-    {name: 'Camisa Polo', price: 109.99, category: "Roupas Esportivas"},
-    {name: 'Calça Jeans', price: 80.99, category: "Roupas"},
-    {name: 'Chaleira elétrica', price: 90.99, category: "Eletrônicos"}
+//     {name: 'Camisa', price: 10.99, category: "Roupas"},
+//     {name: 'Camisa Polo', price: 109.99, category: "Roupas Esportivas"},
+//     {name: 'Calça Jeans', price: 80.99, category: "Roupas"},
+//     {name: 'Chaleira elétrica', price: 90.99, category: "Eletrônicos"}
 
-]
+// ]
 
-produtos.map((product)=>{
+// produtos.map((product)=>{
 
-if(product.category === 'Roupas'){
+// if(product.category === 'Roupas'){
 
-    product.onSale = true
-}
-})
+//     product.onSale = true
+// }
+// })
 
-console.log(produtos)
+// console.log(produtos)
 
 
-const nome = "Nathanzinho Soares";
+// const nome = "Nathanzinho Soares";
 
-const idade = 4;
+// const idade = 4;
 
-console.log(`O ${nome} esta estudado e tem ${idade} anos`)
+// console.log(`O ${nome} esta estudado e tem ${idade} anos`)
+
+const a1 = [1,2,3,4,5,6,7,8,9,10]
+const a2 =[11,12,13,14,15,16,17,18,19,20]
+
+const a3 =[...a1, ...a2];
+
+//console.log(a3)
+
+
+const a5 =[0, 100,300, ...a1, 30,40]
+
+console.log(a5)
