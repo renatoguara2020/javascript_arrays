9f0bade1d51fed5dd584148a855d83914d48488d
Renato Alves Soares <renatoguara2020@gmail.com>
2022-05-30 09:40:54 -0300

    commit changes

--- main.js
+++ main.js
@@ -1,1 +1,60 @@
-console.log('Testes com JavaScript')
+//console.log('Testes com JavaScript')
+
+let nome = "Renato Alves Soares";
+
+//console.log(nome);
+
+let idade = 50;
+
+let profissao = "Analista de Sistemas WEB";
+
+let isWorking = false;
+
+console.log(`Meu nome é: ${nome} e tenho a idade de:${idade} e minha profissão é: ${profissao} e esta trabalhando ${isWorking}`);
+
+
+console.log('=============================================================')
+console.log(true + true)
+console.log(false + false)
+console.log(true + false)
+
+
+// console.log(2**0);
+
+// console.log('Módulo de 3')
+// console.log(10 % 3)
+
+// console.log('Infinity no JS');
+
+// console.log( 10 * Infinity);
+
+console.log(2 == '2');
+
+console.log(2 === '2');
+
+
+console.log('Null and Undefined')
+console.log(!undefined);
+
+console.log(!null)
+console.log(!0)
+console.log('')
+console.log(!"")
+
+
+// console.log('Expressões com && and ||')
+// let exp1 = false;
+// let exp2 = true;
+
+// console.log(exp1 && exp2)
+// console.log(exp1 || exp2)
+// console.log(!exp1 && exp2)
+
+// let nota1 = 9;
+// let nota2 = 8;
+// let nota3 = 7;
+// let nota4 = 10;
+
+// let media = console.log((nota1 + nota2 + nota3 + nota4)/4);
+
+
